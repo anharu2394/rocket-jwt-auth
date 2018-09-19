a53b855418bc2425c96ed071e2c214ce20f4f06a
安藤春香 <haruan2394@gmail.com>
2018-09-19 13:41:23 +0900

    Add: Rocket has launched

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 
+extern crate rocket;
+#[macro_use] extern crate diesel;
+extern crate r2d2;
+extern crate r2d2_diesel;
+mod db;
+mod schema;
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).manage(db::connect()).launch();
 }
